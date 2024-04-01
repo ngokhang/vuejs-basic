6f27bb3097221d329b23203c8162269393642a85
ngokhang <ngokhang2002vn@gmail.com>
2024-04-01 16:49:48 +0700

    first commit

---
+++ src/components/StudentList.vue
@@ -0,0 +1,48 @@
+<template lang="">
+  <div class="student-list">
+    <div
+      v-for="student in studentList"
+      :key="student.id"
+      class="student-list__item"
+    >
+      <p>{{ student.name }}</p>
+      <input
+        type="checkbox"
+        @click="handleSelect(student.id)"
+        v-model="student.selected"
+      />
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    studentList: {
+      type: Array,
+      required: true,
+    },
+  },
+  emits: {
+    selectStudent: {
+      type: Function,
+      required: true,
+    },
+  },
+  methods: {
+    handleSelect(studentId) {
+      this.$emit("selectStudent", this.studentList, studentId);
+    },
+  },
+};
+</script>
+<style scoped>
+.student-list {
+  width: 300px;
+  padding: 5px;
+  border: 1px solid black;
+}
+.student-list__item {
+  display: flex;
+  gap: 10px;
+}
+</style>
---
+++ src/components/User.vue
@@ -0,0 +1,20 @@
+<template lang="">
+  <div>
+    {{ userData.name }}
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    userData: {
+      type: {
+        name: String,
+        avatar: String,
+        id: Number,
+      },
+      required: true,
+    },
+  },
+};
+</script>
+<style lang=""></style>
